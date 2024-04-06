74af081dd2b00bf020603465261653aad68710be
lincoln gibson <lincolngibson7@gmail.com>
2024-04-06 10:46:00 -0300

    fix deployment error raised from typo.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one_attached :avatar
-  has_many :posts, foriegn_key: :author_id
+  has_many :posts, foreign_key: :author_id
   has_many :comments, through: :posts
   has_many :likes, through: :posts
 end
