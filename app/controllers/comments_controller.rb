04695c394ad1c4e82d25a6de1b482bddadbdd497
lincoln gibson <lincolngibson7@gmail.com>
2024-04-08 19:13:35 -0300

    Add comments controller and implement method to create a comment on each post.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    @comment.author = current_user
+    if @comment.save
+      flash[:notice] = "Comment was successfully created"
+    else
+      flash[:alert] = @comment.errors.full_messages.join(", ")
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:thought, :post_id, :author_id)
+  end
+end
