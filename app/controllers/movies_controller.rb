c9650ba83216ea5186638f844802aa123991c29b
lincoln gibson <lincolngibson7@gmail.com>
2024-04-08 19:10:41 -0300

    fully integrate the movie features in the controller and views.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!
+  before_action :set_movie, only: [:show]
+
   def index
     @poster = MoviedbApi.new.poster
     @popular = MoviedbApi.new.popular
@@ -13,8 +15,14 @@ class MoviesController < ApplicationController
     @movie = MoviedbApi.new.get_movie_details(id)
     @location = MoviedbApi.new.service_location
     @providers = MoviedbApi.new.watch_providers(id, location: @location)
-    # @trailer = MoviedbApi.new.play_trailers(id)
-    # @casts = MoviedbApi.new.movie_credits(id)
-    # @recommended = MoviedbApi.new.recommended_movies(id)
+    @trailer = MoviedbApi.new.play_trailers(id)
+    @casts = MoviedbApi.new.movie_credits(id)
+    @recommended = MoviedbApi.new.recommended_movies(id)
+  end
+
+  private
+
+  def set_movie
+    @movie = MoviedbApi.new.get_movie_details(params[:id])
   end
 end
