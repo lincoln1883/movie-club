0b821e481a19701fbaebc0f8def2a5e1a61212c5
lincoln gibson <lincolngibson7@gmail.com>
2024-04-11 00:24:26 -0300

    Add helper methods to the user model and specify the current user to access the data in the profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
+
   def index
     @users = User.all
+  end
+
+  def show
+    current_user
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable
 
-  has_one_attached :avatar
+  has_one_attached :avatar, service: :amazon, dependent: :destroy
   has_many :posts, foreign_key: :author_id, dependent: :destroy
   has_many :comments, through: :posts
   has_many :likes, through: :posts
@@ -13,7 +14,31 @@ class User < ApplicationRecord
   validates :post_count, numericality: {integer: true, greater_than_or_equal_to: 0}
   validates :bio, presence: true, length: {minimum: 5, maximum: 500}
 
+  after_commit :add_default_avatar, on: %i[create update]
+
   def recent_posts
     posts.order(created_at: :desc).limit(3)
+  end
+
+  def avatar_thumbnail
+    if avatar.attached?
+      avatar.variant(resize: '150x150!')
+    else
+      '/default-avatar.jpg'
+    end
+  end
+
+  private
+
+  def add_default_avatar
+    return if avatar.attached?
+    
+    avatar.attach(
+      io: File.open(
+        Rails.root.join('app', 'assets', 'images', 'default-avatar.jpg')
+),
+      filename: 'default-avatar.jpg',
+      content_type: 'image/jpg'
+)
   end
 end
