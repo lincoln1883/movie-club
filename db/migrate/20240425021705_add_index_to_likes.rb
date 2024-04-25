5c0a5600a103cef9341768eab2194bd13f18bba7
lincoln gibson <lincolngibson7@gmail.com>
2024-04-25 18:28:31 -0300

    Add migration to make like unique to one user and post.

---
+++ db/migrate/20240425021705_add_index_to_likes.rb
@@ -0,0 +1,5 @@
+class AddIndexToLikes < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :likes, :user_id, :author_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_06_161856) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_25_021705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "btree_gist"
@@ -78,6 +78,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_06_161856) do
     t.uuid "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["author_id", "post_id"], name: "index_likes_on_author_id_and_post_id", unique: true
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
