214155e44ba1ddb7f5ba37b33812c9a5c791af2c
lincoln gibson <lincolngibson7@gmail.com>
2024-04-25 18:37:05 -0300

    Add likes many to one with the post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   validates :review, presence: true, length: { minimum: 3, maximum: 100 }
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
